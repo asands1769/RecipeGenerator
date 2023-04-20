89209d8351e299504456afe01585b424bbce9d5b
asands1769 <asands1769@outlook.com>
2023-04-20 17:48:20 -0500

    Fixed displayname bug

--- RecipeGeneratorFrontend/src/app/userprofile/userprofile.component.ts
+++ RecipeGeneratorFrontend/src/app/userprofile/userprofile.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UserprofileComponent implements OnInit {
   currentUser: String;
   constructor() { 
-    this.currentUser = history.state.data.username;
+    this.currentUser = history.state.data;
   }
 
   ngOnInit(): void {
